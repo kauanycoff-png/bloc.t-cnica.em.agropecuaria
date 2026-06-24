c5ea2ae10242e148225946a79e2685847cb02d28
kauanycoff-png <kauany.coff@escola.pr.gov.br>
2026-06-24 09:28:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+        "A ovinocultura orgânica promove equilíbrio ambiental, saúde animal e produção sustentável! 🐑🌱";
+}
